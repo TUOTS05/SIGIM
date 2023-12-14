8733eea02cc5e040dd5c9753381f2ab9f3af82a2
Joel-kouame <86069762+Joel-kouame@users.noreply.github.com>
2023-12-14 08:56:41 +0000

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,18 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    // ACCUEIL KOUAME
     public function index(): string
     {
         return view('Frontend/Accueil/index');
     }
+    public function contact(): string
+    {
+        return view('Frontend/Accueil/contact');
+    }
+    public function apropo(): string
+    {
+        return view('Frontend/Accueil/apropos');
+    }
+    // FIN ACCUEIL
 }
